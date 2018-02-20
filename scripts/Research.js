977a1ab7b407d89a5d10a5b68b4b9d43bc27bbe0
FedericoPerra <federicoperra8@gmail.com>
2018-02-20 14:17:57 +0100

    commit con research funzionante con controllo onkeyup

--- scripts/Research.js
+++ scripts/Research.js
@@ -1,17 +1,17 @@
 function Ricerca()
 {
     reset();
-    var stringa=document.getElementById("research").value;
-    var table = document.getElementById("id_table");
-    var celle = table.getElementsByTagName("td");
-    for(var j=0; j<celle.length; j++){
-        var n = celle[j].search(stringa);
-        if(n<0){
-            celle[j].style.backgroundColor="yellow";
+    var stringa = document.getElementById("research").value;
+    if(stringa!="") {
+        var table = document.getElementById("id_table");
+        var celle = table.getElementsByTagName("td");
+        for (var j = 0; j < celle.length; j++) {
+            if (celle[j].innerHTML.search(stringa) != -1 && celle[j].innerHTML.search("<") == -1) {
+                celle[j].style.backgroundColor = "yellow";
+            }
         }
     }
 }
-
 
 function reset() {
     var table = document.getElementById("id_table");
